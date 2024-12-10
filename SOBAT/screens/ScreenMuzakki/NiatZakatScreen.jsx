6caa3fc82eb63e7feff2a9486a538be22395be9c
TopikRoman <rohmantaufik327@gmail.com>
2024-12-10 16:09:05 +0700

    update again :(

--- SOBAT/screens/ScreenMuzakki/NiatZakatScreen.jsx
+++ SOBAT/screens/ScreenMuzakki/NiatZakatScreen.jsx
@@ -1,42 +1,31 @@
 import React from "react";
 import { View, Image, StyleSheet, ScrollView } from "react-native";
 
-export default function NiatZakatScreen() {
+const ImageComponent = ({ source }) => (
+  <Image source={source} style={styles.image} />
+);
+
+const imageSources = [
+  require("../../assets/Niat Zakat/Niat Anak Laki Laki.png"),
+  require("../../assets/Niat Zakat/Niat Anak Perempuan.png"),
+  require("../../assets/Niat Zakat/Niat Diwakilkan.png"),
+  require("../../assets/Niat Zakat/Niat Mewakilkan.png"),
+  require("../../assets/Niat Zakat/Niat Pribadi.png"),
+  require("../../assets/Niat Zakat/Niat Sekeluarga.png"),
+  require("../../assets/Niat Zakat/Niat Suami Istri.png"),
+];
+
+const NiatZakatScreen = () => {
+  const renderImages = imageSources.map((source, index) => (
+    <ImageComponent key={index} source={source} />
+  ));
+
   return (
     <ScrollView style={styles.container}>
-      <View style={styles.imageContainer}>
-        <Image
-          source={require("../../assets/Niat Zakat/Niat Anak Laki Laki.png")}
-          style={styles.image}
-        />
-        <Image
-          source={require("../../assets/Niat Zakat/Niat Anak Perempuan.png")}
-          style={styles.image}
-        />
-        <Image
-          source={require("../../assets/Niat Zakat/Niat Diwakilkan.png")}
-          style={styles.image}
-        />
-        <Image
-          source={require("../../assets/Niat Zakat/Niat Mewakilkan.png")}
-          style={styles.image}
-        />
-        <Image
-          source={require("../../assets/Niat Zakat/Niat Pribadi.png")}
-          style={styles.image}
-        />
-        <Image
-          source={require("../../assets/Niat Zakat/Niat Sekeluarga.png")}
-          style={styles.image}
-        />
-        <Image
-          source={require("../../assets/Niat Zakat/Niat Suami Istri.png")}
-          style={styles.image}
-        />
-      </View>
+      <View style={styles.imageContainer}>{renderImages}</View>
     </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -58,3 +47,5 @@ const styles = StyleSheet.create({
     marginTop: -50,
   },
 });
+
+export default NiatZakatScreen;
